227aa8e22e6e6d7428f1fd382c9d35fc2dff47aa
USER <USER@DESKTOP-RVQ72UJ>
2021-12-12 22:21:34 +0800

    jhjh

--- ASSISGNMENT/src/assisgnment/ASSISGNMENT.java
+++ ASSISGNMENT/src/assisgnment/ASSISGNMENT.java
@@ -14,7 +14,7 @@ public class ASSISGNMENT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("fuck leham");
+        System.out.println("fuck");
     }
     
 }
