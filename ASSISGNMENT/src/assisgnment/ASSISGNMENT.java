1bc5118caee8420a5273f2c8e6dd6a52546b8baa
shazwydaniel <shazwydaniel@localhost>
2021-12-12 23:50:08 +0800

    shafiq breedable

--- ASSISGNMENT/src/assisgnment/ASSISGNMENT.java
+++ ASSISGNMENT/src/assisgnment/ASSISGNMENT.java
@@ -14,7 +14,7 @@ public class ASSISGNMENT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("fuck cpg");
+        System.out.println("fuck shafiq :D");
     }
     
 }
