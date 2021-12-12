8a9e48f58e015d03e1393d882810d866039b88c5
USER <USER@DESKTOP-RVQ72UJ>
2021-12-12 23:14:14 +0800

    LOGIN FILE

---
+++ ASSISGNMENT/src/assisgnment/login.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assisgnment;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class login {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        String username;
+        String password;
+        System.out.print("Enter username: ");
+        username = input.next();
+        System.out.print("Enter password: ");
+        password = input.next();
+        
+        // enter database
+//        try (Connection connection = DriverManager.getConnection(url/* url database */, username, password)) {
+//        System.out.println("Database connected!");
+//        } catch (SQLException e) {
+//        throw new IllegalStateException("Cannot connect the database!", e);
+}
+        
+        
+        
+        
+    }
+    
+}
