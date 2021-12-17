7fbb9b62acfb09541fc3564865875ac6f89988c1
CAPANG JABBA <ihaziq18.ih@gmail.com>
2021-12-18 00:07:50 +0800

    not done this shit hard

--- ASSISGNMENT/src/labreport/q4.java
+++ ASSISGNMENT/src/labreport/q4.java
@@ -10,6 +10,7 @@ public class q4 {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter number of elements : ");
         length = input.nextInt();
+
         int[][] a = new int[2][length];
         for (int i = 0;i<2;i++){
             for (int j = 0;j<length;j++){
@@ -19,7 +20,7 @@ public class q4 {
             }
         }
         int [][] ans = new int[length][length];
-        int [] location = new int[length];
+        int[] coordinate = {100,100,100,100,100};
         for(int i = 0;i<length;i++){
             for (int j = 0;j<length;j++){
                 ans[i][j] = a[0][i]^a[1][j];
@@ -27,33 +28,10 @@ public class q4 {
             }
             System.out.println();
         }
-        for (int i = 0;i<length;i++){
-            for (int j = 0;j<length;j++){
-                if (j==i){
-                    continue;
-                }
-                for (int k = 0;k<length;k++){
-                    if (k==i || k==j){
-                        continue;
-                    }
-                    for (int l = 0;l<length;l++){
-                        if (l==i || l==j || l==k){
-                            continue;
-                        }
-                        for (int m = 0;m<length;m++){
-                            if (m==i || m==j || m==k || m==l){
-                                continue;
-                            }
-                            sum = ans[0][i]+ ans[1][j] + ans[2][k] + ans[3][l] + ans[4][m];
-                            if (sum>max_sum){
-                                max_sum=sum;
-                                System.out.println("coordinate: "+i+j+k+l+m);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        // the algorithm for max sum
+
+
+
         System.out.println("max sum : "+ max_sum);
     }
 }
