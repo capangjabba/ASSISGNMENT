e7d7631421a42a8e27ea5c40a09090ad908b358a
CAPANG JABBA <ihaziq18.ih@gmail.com>
2021-12-18 16:21:38 +0800

    DONE LOGIN

--- ASSISGNMENT/src/assisgnment/login.java
+++ ASSISGNMENT/src/assisgnment/login.java
@@ -4,9 +4,7 @@
  */
 package assisgnment;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Scanner;
 
 /**
@@ -14,10 +12,25 @@ import java.util.Scanner;
  * @author USER
  */
 public class login {
-    public static void main(String[] args) {
 
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/capang_screen_cinema", "root", "18102002")) {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        try  {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/capang_screen_cinema", "root", "18102002");
             System.out.println("Database connected!");
+            Statement myStmt = connection.createStatement();
+            ResultSet myRs = myStmt.executeQuery("SELECT * FROM guest");
+            System.out.print("Enter username : ");
+            String username = input.next();
+            System.out.print("Enter password : ");
+            String password = input.next();
+            while (myRs.next()) {
+                if (myRs.getString("username").equals(username) && myRs.getString("password").equals(password)) {
+                    System.out.println("WE IN");
+                }
+            }
+
         } catch (
                 SQLException e) {
             throw new IllegalStateException("Cannot connect the database!", e);
