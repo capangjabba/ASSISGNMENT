83a8d7725cb83c5d2a9c8abb347d9a1bd012f7a4
CAPANG JABBA <ihaziq18.ih@gmail.com>
2021-12-18 18:38:24 +0800

    DONE loginGuest AND registerGuest

--- ASSISGNMENT/src/assisgnment/registerGuest.java
+++ ASSISGNMENT/src/assisgnment/registerGuest.java
@@ -8,15 +8,17 @@ public class registerGuest {
         Scanner input = new Scanner(System.in);
 
         try  {
+            // CREATE CONNECTION WITH DATABASE
             Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/capang_screen_cinema", "root", "18102002");
             System.out.println("Database connected!");
+            // CREATE STATEMENT FOR MYSQL
             Statement myStmt = connection.createStatement();
 
             System.out.print("Enter username : ");
             String username = input.next();
             System.out.print("Enter password : ");
             String password = input.next();
-
+            // CREATE QUERY INSTRUCTIONS
             String sql = "INSERT INTO guest "
                     + "(username,password)"
                     + "values ('"+username+"','"+password+"')";
