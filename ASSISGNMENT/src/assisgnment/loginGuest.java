77731aa83f48ab0fb5ec01a4afdd2058b1c0780c
CAPANG JABBA <ihaziq18.ih@gmail.com>
2021-12-18 16:35:28 +0800

    DONE LOGIN

---
+++ ASSISGNMENT/src/assisgnment/loginGuest.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assisgnment;
+
+import java.sql.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class loginGuest {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        try  {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/capang_screen_cinema", "root", "18102002");
+            System.out.println("Database connected!");
+            Statement myStmt = connection.createStatement();
+            ResultSet myRs = myStmt.executeQuery("SELECT * FROM guest");
+            System.out.print("Enter username : ");
+            String username = input.next();
+            System.out.print("Enter password : ");
+            String password = input.next();
+            while (myRs.next()) {
+                if (myRs.getString("username").equals(username) && myRs.getString("password").equals(password)) {
+                    System.out.println("WE IN"); // got into system
+                }
+            }
+
+        } catch (
+                SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
+    }
+}
+    
+
