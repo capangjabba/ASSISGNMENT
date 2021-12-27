24cd96fec5ca7f8b79f23b4067d820d4e9f23cc2
Acer <Acer@LAPTOP-UB2P8VFB>
2021-12-27 11:19:33 +0800

    Q3 tp tak sure betul ke tak

--- ASSISGNMENT/src/labreport/q3.java
+++ ASSISGNMENT/src/labreport/q3.java
@@ -5,45 +5,73 @@ import java.util.*;
 
 public class q3 {
   
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.print("s: ");
     String s = scanner.nextLine();
     System.out.print("k: ");
     int k = scanner.nextInt();
-    int first = 0;
-    int last = 0;
-  
-    
-    for(int i = 0; i< s.length(); i++){
-        if(i== 8){
-        break;
-        }
-        if(s.charAt(i) == '?'){
-            if(i==0){
-            first = 1;
-            last = 9;
-            }
-            else{
-            first = first*10;
-            last = last*10 + 9;
-            }
-        }
-        else{
-        first = first*10 + Character.getNumericValue(s.charAt(i));
-        last = last*10 + Character.getNumericValue(s.charAt(i));
-        }
-    }   
-    
-    int count = 0;
-    for(int i= first; i<= last; i++){
-        if(i%k == 0){
-        count++;
-        }
+    int d = swapNum( s , k);
+    System.out.println(d);
+		
     }
-    
-    System.out.print(count);
-
-   }
-}  
+	
+	public static int swapNum(String s, int k) {
+		
+		int c=0;
+		String temp1, temp2, temp3;
+		boolean f1 = false, f2=false, f3=false;
+		
+		for(int i=0; i < s.length(); i++) { // loop
+			if(s.charAt(i) == '?') {
+				f1 = true;
+				for(int j=0; j <= 9; j++) { // swap
+					if(i == 0 && j==0);
+					else {
+						temp1 = s.substring(0, i) + j + s.substring(i+1);
+						//System.out.println(i + ": " + temp1);
+						for(int l=i+1; l < s.length(); l++) { // loop
+							if(temp1.charAt(l) == '?') {
+								f2 = true;
+								for(int m=0; m <= 9; m++) { // swap
+									temp2 = temp1.substring(0, l) + m + temp1.substring(l+1);
+									//System.out.println(i + ": " + temp2);
+									for(int n=l+1; n < s.length(); n++) { // loop
+										if(temp2.charAt(n) == '?') {
+											f3 = true;
+											for(int o=0; o <= 9; o++){ // swap
+												temp3 = temp2.substring(0, n) + o + temp2.substring(n+1);
+												//System.out.println(i + ": " + temp3);
+												if(Integer.parseInt(temp3) % k == 0) {
+													c++;
+												}
+											}
+										}
+									}
+									if(!f3) {
+										if(Integer.parseInt(temp2) % k == 0) {
+											c++;
+										}
+									}
+								}
+							}
+						}
+						if(!f2) {
+							if(Integer.parseInt(temp1) % k == 0) {
+								c++;
+							}
+						}
+					}
+				}
+			}
+		}
+		if(!f1) {
+			if(Integer.parseInt(s) % k == 0) {
+				c++;
+			}
+		}
+		return c;
+	}
+}
 
