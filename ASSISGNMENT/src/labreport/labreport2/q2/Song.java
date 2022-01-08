c578c01cbffe152989a103e1ef503acffaed9e72
CAPANG JABBA <ihaziq18.ih@gmail.com>
2022-01-09 02:23:26 +0800

    Song

--- ASSISGNMENT/src/labreport/labreport2/q2/Song.java
+++ ASSISGNMENT/src/labreport/labreport2/q2/Song.java
@@ -1,4 +1,136 @@
 package labreport.labreport2.q2;
 
+import java.util.Arrays;
+
 public class Song {
+    private static String song[][] = new String[20][5];
+
+    private static int i=0;
+    private static int j=0;
+
+    public Song(String title, String filename, String singer, String duration, String year){
+        this.song[i][0]=title;
+        this.song[i][1]=filename;
+        this.song[i][2]=singer;
+        this.song[i][3]=duration;
+        this.song[i][4]=year;
+
+        ++i;
+    }
+
+    public void sortTitle(){
+        for (int a=0;a<i-1;a++){
+            for (int b=0;b<i-a-1;b++){
+                if (Character.compare(song[b][0].charAt(0),song[b+1][0].charAt(0))>0){
+                    String temp[]=song[b];
+                    song[b]=song[b+1];
+                    song[b+1]=temp;
+                }
+            }
+        }
+
+
+
+        System.out.println(toString());
+    }
+
+    public void sortDuration(){
+        for (int a=0;a<i-1;a++){
+            for (int b=0;b<i-a-1;b++){
+
+                if (Double.valueOf(song[b][3])>Double.valueOf(song[b+1][3])){
+                    String temp[]=song[b];
+                    song[b]=song[b+1];
+                    song[b+1]=temp;
+                }
+            }
+
+        }
+
+        System.out.println(toString());
+    }
+
+    public String searchTitle(String title){
+        String bruh="";
+        for (int a=0;a<20;a++){
+            if (title.equals(song[a][0])){
+                for (int b=0;b<5;b++){
+                    bruh+=song[a][b]+" ";
+                }
+            }
+        }
+        return bruh;
+    }
+
+    public String searchSinger(String singer){
+        String bruh="";
+        for (int a=0;a<20;a++){
+            if (singer.equals(song[a][2])){
+                for (int b=0;b<5;b++){
+                    bruh+=song[a][b]+" ";
+                }
+            }
+        }
+        return bruh;
+    }
+
+    public String play(){
+        String bruh="";
+        for (int a=0;a<5;a++){
+            bruh+=song[j][a]+" ";
+        }
+        if (j==20){
+            j=0;
+        }else {
+            j++;
+        }
+        return bruh;
+    }
+    public String next(){
+        j++;
+        if (j>20){
+            j=0;
+        }
+        String bruh="";
+        for (int a=0;a<5;a++){
+            bruh+=song[j][a]+" ";
+        }
+        if (j==20){
+            j=0;
+        }else {
+            j++;
+        }
+        return bruh;
+    }
+
+    public String previous(){
+        j--;
+        if (j<0){
+            j=20;
+        }
+        String bruh="";
+        for (int a=0;a<5;a++){
+            bruh+=song[j][a]+" ";
+        }
+        if (j==20){
+            j=0;
+        }else {
+            j++;
+        }
+        return bruh;
+    }
+
+
+    public String toString(){
+        String lesgo="";
+        for (int a=0;a<i;a++){
+            for (int b=0;b<5;b++){
+
+                lesgo+=song[a][b]+" ";
+            }
+            lesgo+="\n";
+        }
+        return lesgo;
+    }
+
 }
--- ASSISGNMENT/src/labreport/labreport2/q2/q2.java
+++ ASSISGNMENT/src/labreport/labreport2/q2/q2.java
@@ -2,6 +2,18 @@ package labreport.labreport2.q2;
 
 public class q2 {
     public static void main(String[] args) {
+        Song a1 = new Song("NDA","mp4","BILLIE EILISH","4.40","2021");
+        Song a4 = new Song("WITHOUT YOU","mp4","EMINEM","6.20","2018");
+        Song a2 = new Song("STARBOY","mp4","THE WEEKND","2.40","2018");
+        Song a3 = new Song("OFF THE GRID","mp4","KANYE","3.20","2018");
+
+
+
+
+        System.out.println(a2.toString());
+
+        a3.sortTitle();
+        a3.sortDuration();
 
     }
 }
