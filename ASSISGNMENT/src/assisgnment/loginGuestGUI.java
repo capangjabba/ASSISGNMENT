0ebd7aa7b5a05670ac67820bcebbcd6c2564c6f1
CAPANG JABBA <ihaziq18.ih@gmail.com>
2022-01-12 10:59:58 +0800

    done register guest

--- ASSISGNMENT/src/assisgnment/loginGuestGUI.java
+++ ASSISGNMENT/src/assisgnment/loginGuestGUI.java
@@ -143,6 +143,9 @@ public class loginGuestGUI extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        registerGuestGUI a = new registerGuestGUI();
+        a.show();
+        dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
