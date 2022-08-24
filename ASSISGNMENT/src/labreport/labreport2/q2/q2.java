235301d1555e629ddbf98625dc785fe5f3a37fdd
CAPANG JABBA <ihaziq18.ih@gmail.com>
2022-08-25 01:11:41 +0800

    yes

--- ASSISGNMENT/src/labreport/labreport2/q2/q2.java
+++ ASSISGNMENT/src/labreport/labreport2/q2/q2.java
@@ -1,26 +1,26 @@
-package labreport.labreport2.q2;
-
-public class q2 {
-    public static void main(String[] args) {
-        Song a1 = new Song("NDA","mp4","BILLIE EILISH","4.40","2021");
-        Song a4 = new Song("WITHOUT YOU","mp4","EMINEM","6.20","2018");
-        Song a2 = new Song("STARBOY","mp4","THE WEEKND","2.40","2018");
-        Song a3 = new Song("OFF THE GRID","mp4","KANYE","2.20","2018");
-
-
-
-
-        System.out.println(a2.toString());
-
-        a3.sortTitle();
-        a3.sortDuration();
-        System.out.println(a2.searchTitle("NDA"));
-        System.out.println();
-
-        System.out.println(a2.next());
-        System.out.println(a2.next());
-        System.out.println(a2.next());
-        System.out.println(a2.next());
-
-    }
-}
+//package labreport.labreport2.q2;
+//
+//public class q2 {
+//    public static void main(String[] args) {
+//        Song a1 = new Song("NDA","mp4","BILLIE EILISH","4.40","2021");
+//        Song a4 = new Song("WITHOUT YOU","mp4","EMINEM","6.20","2018");
+//        Song a2 = new Song("STARBOY","mp4","THE WEEKND","2.40","2018");
+//        Song a3 = new Song("OFF THE GRID","mp4","KANYE","2.20","2018");
+//
+//
+//
+//
+//        System.out.println(a2.toString());
+//
+//        a3.sortTitle();
+//        a3.sortDuration();
+//        System.out.println(a2.searchTitle("NDA"));
+//        System.out.println();
+//
+//        System.out.println(a2.next());
+//        System.out.println(a2.next());
+//        System.out.println(a2.next());
+//        System.out.println(a2.next());
+//
+//    }
+//}
